d88d923b93eaaff358c73afda30b3ad505d2aec2
Maxime Laudrin <maximedelaudrin@gmail.com>
2015-10-20 14:24:22 +0200

    Added qrec.zsh in bin/

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(name='qdrill',
       version='0.0.0',
-      description='Quickly record list of short sound files',
-      url='http://www.github.com/gropax/qrec.py',
+      description='Generate drill audio file for language learning',
+      url='http://www.github.com/gropax/qdrill.py.git',
       author='gropax',
       author_email='maximedelaudrin@gmail.com',
       license='MIT',
-      packages=['qrec'],
-      scripts=['bin/qrec'],
+      packages=['qdrill'],
+      scripts=['bin/qdrill', 'bin/qrec'],
       zip_safe=False)
